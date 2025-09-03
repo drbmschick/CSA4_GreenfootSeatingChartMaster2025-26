bd003340dc423204dcf3f7c82da03b1e84e08050
AdithyaGnanasundar <gnanasundar2992@mydusd.org>
2025-09-03 11:21:34 -0700

    Update Classroom.java

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+     
+        AdithyaGnanasundar adithyagnanasundar = new AdithyaGnanasundar();
+        addObject(adithyagnanasundar,9,10);
+        adithyagnanasundar.assignSeat();
   
     }
     
