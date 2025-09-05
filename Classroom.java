8a3de3b7b7d2e6f19747c94f146a51fcbe0bdb6e
ChloeBarlow <barlow4061@mydusd.org>
2025-09-05 10:55:05 -0700

    Add files via upload

--- Classroom.java
+++ Classroom.java
@@ -146,12 +146,16 @@ public class Classroom extends World
     private void prepare()
     {
         createDeskLayout();
- // Each student needs to create their specific instance following the KilgoreTrout example.
- // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
- // and then clicking on the inspect text
+         // Each student needs to create their specific instance following the KilgoreTrout example.
+         // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
+         // and then clicking on the inspect text
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        BarlowChloe barlowChloe = new BarlowChloe();
+        addObject(barlowChloe,9,3);
+        barlowChloe.assignSeat();
   
     }
     
