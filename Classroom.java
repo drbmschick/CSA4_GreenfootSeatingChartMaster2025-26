f59900a480e74d7b05ab7a85d8fc6275107f91de
unknown <revanthguda4@gmail.com>
2025-09-12 10:57:51 -0700

    I finished

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        RevanthGuda revanthguda = new RevanthGuda();
+        addObject(revanthguda,6,9);
+        revanthguda.assignSeat();
   
     }
     
--- RevanthGuda.java
+++ RevanthGuda.java
@@ -12,15 +12,6 @@ public class RevanthGuda extends Student implements SpecialInterestOrHobby
 
     /**
      * Constructor for the RevanthGuda class.
-     * Constructors are special methods with the same exact name as the class name.  
-     * Constructors to not have return types.
-     * Constructors can be overloaded. This means we can call a constructor with different sets of parameter
-     *  lists to initalize for different conditions (depending on what constructors have been written.
-     * @param String f (firstname)
-     * @param String l (lastname)
-     * @param int r (row of seating arrangement)
-     * @param int s (seat number within row seating arrangement)
-     * 
      */
     public RevanthGuda(String f, String l, int r, int s) {
         firstName=f;
@@ -29,24 +20,22 @@ public class RevanthGuda extends Student implements SpecialInterestOrHobby
         mySeatY=s;
         portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
         standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
-        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
+        soundFile=f.toLowerCase()+l.toLowerCase()+".mp3";  // Make sure to name your sound files firstlast.mp3, all lowercase!!!
         setImage(portraitFile);
         sitting=true;
     }
+    
     /**
-     * Default constructor, if you don't pass in a name and seating location
-     * Pay attention to how the row and seat variables set the location of the image.  1,1 is the first cell in the upper left
-     * of the classroom.
+     * Default constructor - Fixed coordinates to match your seating position
      */
     public RevanthGuda() {
-        firstName="Kilgore";
-        lastName="Trout";
-        mySeatX=1;
-        mySeatY=1;
-       // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
-       portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
-       standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
-        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
+        firstName="Revanth";
+        lastName="Guda";
+        mySeatX=6;  // Fixed: matches your actual seat position
+        mySeatY=9;  // Fixed: matches your actual seat position
+        portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
+        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
+        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".mp3";
         setImage(portraitFile);
         sitting=true;
     }
@@ -59,23 +48,19 @@ public class RevanthGuda extends Student implements SpecialInterestOrHobby
     {
         // Add your action code here.
         if(Greenfoot.mouseClicked(this)){
-          //  if (sitting){
-                sitting=false;
-                setImage(standingFile);
-                System.out.println(""); // Print a blank line to create space between any student output.
-                getName();
-                sayName(soundFile);
-            
-                myHobby("I like to time travel!");
-            // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
-            // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
-            // Call the sitDown() method to move back  to your seat
-            
-                circleClass();  // Kilgore Trount's special method... Please write one of your own. You can use this, but please modify it and be creative.
-           
-                sitDown();
-            }
+            sitting=false;
+            setImage(standingFile);
+            System.out.println(""); // Print a blank line to create space between any student output.
+            getName();
+            sayName(soundFile);  // Fixed: use the soundFile variable instead of hardcoded string
         
+            myHobby("I like to code in my freetime!");
+            
+            // Your custom animation
+            circleClass();  
+       
+            sitDown();
+        }
     } 
     
     /**
@@ -84,51 +69,48 @@ public class RevanthGuda extends Student implements SpecialInterestOrHobby
     public void getName(){
         System.out.println("My name is " + firstName + " " + lastName);
     }
-    /**
-     * This method needs to allow the user to interact with the student through a question and answer interface, and provide
-     * some mechanism that allows the student to sit down once the Q&A session ends.  You can use this basic model, or come up
-     * with some additional class and object that represents a blackboard, or a talking cartoon bubble etc. If you provide extra
-     * classes, make sure to fully document so other students can use the same interface.
-     */
 
-   
     /**
-     * This is a local method specific to the RevanthGuda class used to animate the character once the image is clicked on.
-     * You should write your own methods to perform your own animation for your character/avatar.
+     * Custom animation method - moves around the classroom in a rectangular path
      */
     public void circleClass(){
-        setLocation(0,0);
-         Greenfoot.delay(10);
-        // move right
-        for (int i=1;i<=9;i++){
-            setLocation(i,0);
+        // Save original position
+        int originalX = getX();
+        int originalY = getY();
+        
+        // Start from current position and move in a rectangle
+        // Move right
+        for (int i = originalX; i <= originalX + 4; i++){
+            setLocation(i, originalY);
             Greenfoot.delay(10);
         }
-        // move back
-        for (int i=1;i<=5;i++){
-            setLocation(9,i);
+        
+        // Move down
+        for (int i = originalY; i <= originalY + 3; i++){
+            setLocation(originalX + 4, i);
             Greenfoot.delay(10);
         }      
-         // move left
-        for (int i=9;i>=0;i--){
-            setLocation(i,5);
+        
+        // Move left
+        for (int i = originalX + 4; i >= originalX; i--){
+            setLocation(i, originalY + 3);
             Greenfoot.delay(10);
         }      
-              // move Forward
-        for (int i=5;i>=0;i--){
-            setLocation(0,i);
+        
+        // Move up
+        for (int i = originalY + 3; i >= originalY; i--){
+            setLocation(originalX, i);
             Greenfoot.delay(10);
         }   
-           Greenfoot.delay(20);
-           returnToSeat();
+        
+        Greenfoot.delay(20);
+        returnToSeat();
     }
+    
      /**
      * myHobby is one of the interfaces provided.  
-     * An interface is just a contract for the methods that you will implement in your code.  The College Board no longer
-     * tests on abstract classes and interfaces, but it is good to know about them
      */
      public void myHobby(String s) {
          System.out.println(s);
-}
-
+    }
 }
